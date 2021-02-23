5c4fce222dbdde5d2c7f4e087011d1b095e2478d
Kevin Tarrant <kmtarrant13@gmail.com>
2021-02-23 02:36:52 -0500

    successfully selected and loaded option chain based on earnings date expiration estimate

--- backtrader_ib_api/tools/download.py
+++ backtrader_ib_api/tools/download.py
@@ -25,6 +25,8 @@ if __name__ == "__main__":
     import argparse
     import datetime
 
+    from backtrader_ib_api.finviz import estimate_next_earnings_date
+
     parser = argparse.ArgumentParser(description="""
     Downloads data using the IB Trader Workstation API
     """)
@@ -35,8 +37,9 @@ if __name__ == "__main__":
     parser.add_argument("--clientid", default=0, type=int)
     parser.add_argument("--storage-path", default='C:/stores', help="Path to store downloaded data")
     parser.add_argument("--tickers", default="aapl", help="Comma-separated, case-insensitive list of tickers")
+    parser.add_argument("--exchange", default="SMART", help="Exchange for the ticker")
     parser.add_argument("--bar-size", default="30 mins", help="Bar size")
-    parser.add_argument("--equity-history", default=5, help="Number of days to collect")
+    parser.add_argument("--history-length", default=5, help="Number of days to collect history data")
 
     args = parser.parse_args()
 
@@ -53,21 +56,58 @@ if __name__ == "__main__":
     wrapper.start_app(args.host, args.port, args.clientid)
 
     bar_size_str = args.bar_size.replace("mins", "m").replace(" ", "")
+    duration = f"{args.history_length} d"
+
     equity_history_collection = store.collection(f"trades-{bar_size_str}")
+    option_history_collection = store.collection(f"optiontrades-{bar_size_str}")
 
     tickers = [ticker.upper() for ticker in args.tickers.split(",")]
     for ticker in tickers:
         stock_details = wrapper.request_stock_details(ticker)
-        logger.info(f"Found Match for ticker {ticker}:\n{stock_details}")
+        logger.info(f"Using first match found for ticker {ticker}:\n{stock_details}")
+        stock_contract_id = stock_details.index[0]
 
-        if args.equity_history:
-            duration = f"{args.equity_history} d"
-            history = wrapper.request_stock_trades_history(ticker, duration=duration, bar_size=args.bar_size)
-            logger.info(f"Equity history: {history}")
-            try:
-                # pystore should automatically drop any duplicates, updating the data with latest if there are any
-                equity_history_collection.append(ticker, history)
-            except ValueError:
-                equity_history_collection.write(ticker, history)
+        history = wrapper.request_stock_trades_history(ticker, duration=duration, bar_size=args.bar_size)
+        logger.debug(f"Equity history: {history}")
+        try:
+            # pystore should automatically drop any duplicates, updating the data with latest if there are any
+            equity_history_collection.append(ticker, history)
+        except ValueError:
+            equity_history_collection.write(ticker, history)
+
+        option_params = wrapper.request_option_params(ticker, stock_contract_id)
+        logger.info(f"Found {len(option_params)} option param results")
+
+        # try to auto-select SMART exchange
+        preferred_contracts = option_params[option_params.exchange == args.exchange]
+        option_contract = preferred_contracts.iloc[0]
+
+        # find expiration just after earnings
+        earnings_date = estimate_next_earnings_date(ticker)
+        expiration_dates = [datetime.datetime.strptime(expiration, "%Y%m%d")
+                            for expiration in option_contract.expirations]
+        earnings_expiration_date = min(expiration for expiration in expiration_dates if expiration > earnings_date)
+        logger.info(f"Closest Options Expiration for Earnings: {earnings_expiration_date}")
+
+        earnings_expiration = datetime.datetime.strftime(earnings_expiration_date, "%Y%m%d")
+        option_chain = wrapper.request_option_chain(ticker, option_contract.exchange, earnings_expiration)
+        logger.info(f"Option chain for {ticker} expiration {earnings_expiration}:\n{option_chain}")
+
+        # for contract_id in option_chain.index:
+        #     strike = option_chain.loc[contract_id, "strike"]
+        #     right = option_chain.loc[contract_id, "right"]
+        #     history = wrapper.request_option_trades_history(ticker,
+        #                                                     earnings_expiration,
+        #                                                     strike,
+        #                                                     right,
+        #                                                     duration=duration,
+        #                                                     bar_size=args.bar_size)
+        #     item_name = f"{ticker}-{earnings_expiration}-{strike}{right}"
+        #     logger.debug(f"{item_name} history: {history}")
+        #     try:
+        #         # pystore should automatically drop any duplicates, updating the data with latest if there are any
+        #         option_history_collection.append(ticker, history)
+        #     except ValueError:
+        #         option_history_collection.write(ticker, history)
 
     wrapper.stop_app()
